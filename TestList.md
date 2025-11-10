95a3311ee3310c9a9cfc99c0f4cf213d81233ad0
2danfer2@gmail.com <jeison.fernandez@sinco.com.co>
2025-11-10 16:50:52 -0500

    se agrega Si_SoloElProducctoArrozSeAgregAlCarrito_Debe_ElCarritoDeComprasTenerElPrecioTotalDe1

--- TestList.md
+++ TestList.md
@@ -1,3 +1,4 @@
 - [ ] Si_SoloElProducctoArrozSeAgrega_Debe_ElCarritoDeComprasTenerElValorDelArroz
 - [ ]  Un producto cuesta su precio unitario
+- [ ] los productos no pueden tener valores negativos
 
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,4 +1,5 @@
 ﻿using AwesomeAssertions;
+using System.Linq;
 
 namespace SuperMarketTDD;
 
@@ -8,17 +9,53 @@ public class UnitTest1
     public void Si_ElCarritoDeComprasEstaVacio_Debe_ElCarritoDeComprasTenerElPrecioTotalDe0()
     {
         var carritoCompras = new CarritoDeCompras();
-
         var precioTotalEnCarritoDeCompras = carritoCompras.PrecioTotal();
 
         precioTotalEnCarritoDeCompras.Should().Be(0);
     }
+    
+    [Fact]
+    public void Si_SoloElProducctoArrozSeAgregAlCarrito_Debe_ElCarritoDeComprasTenerElPrecioTotalDe1()
+    {
+        var producto = new Producto("Arroz",1);
+        var carritoCompras = new CarritoDeCompras();
+        carritoCompras.AgregarProducto(producto);
+
+        var precioTotalEnCarritoDeCompras = carritoCompras.PrecioTotal();
+
+        precioTotalEnCarritoDeCompras.Should().Be(1);
+    }
+}
+
+public class Producto
+{
+    
+    private string _producto { get; set; }
+    private int _valorUnidad { get; set; }
+
+    public Producto(string producto, int valorUnidad)
+    {
+        _producto = producto;
+        _valorUnidad = valorUnidad;
+    }
+
+    public int ObtenerValorUnidad()
+    {
+        return _valorUnidad;
+    }
+
 }
 
 public class CarritoDeCompras
 {
+    List<Producto> _productos = [];
     public int PrecioTotal()
     {
-        return 0;        
+        return _productos.Any() ? _productos.Sum(p => p.ObtenerValorUnidad()) : 0;
+    }
+
+    public void AgregarProducto(Producto producto)
+    {
+        _productos.Add(producto);
     }
 }
